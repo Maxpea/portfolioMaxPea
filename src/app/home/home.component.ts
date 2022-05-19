250ba4925df512ea3c93f0bffce3ebabd17fdf38
MaxPea <max.pean807@gmail.com>
2022-05-19 15:45:53 +0200

    maj

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import axios, { AxiosError } from 'axios';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+htmlData:any;
 
   constructor() { }
 
   ngOnInit(): void {
+/*
+    this.htmlData=this.FetchPage("https://www.codingame.com/profile/87d78b40758ce6fafda74f50afe159fa0702984")
+    console.log(this.htmlData)
+  }
+
+  FetchPage(url: string): Promise<string | undefined> {
+    const HTMLData = axios
+      .get(url)
+      .then(res => res.data)
+      .catch((error: AxiosError) => {
+        console.error(`There was an error with ${error.config.url}.`);
+        console.error(error.toJSON());
+      });
+
+    return HTMLData;
+    */
   }
 
 }
